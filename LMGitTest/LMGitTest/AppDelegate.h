798b128e24338c63f4ab645552d673879f981bfb
leiming <2982283054@qq.com>
2018-12-29 15:59:37 +0800

    项目初始化

---
+++ LMGitTest/LMGitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LMGitTest
+//
+//  Created by 雷明 on 2018/12/29.
+//  Copyright © 2018年 leiming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LMGitTest/LMGitTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LMGitTest
+//
+//  Created by 雷明 on 2018/12/29.
+//  Copyright © 2018年 leiming. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
